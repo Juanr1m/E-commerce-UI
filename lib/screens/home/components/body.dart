6aab64212d9a04144fb65088bf2215182d9c3517
Rim Karimov <rimkarimov20@gmail.com>
2020-09-11 16:36:04 +0500

    home screen todo

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_way/constants.dart';
+import 'package:flutter_way/models/product.dart';
+
 import 'package:flutter_way/screens/home/components/discount_banner.dart';
+import 'package:flutter_way/screens/home/components/section_title.dart';
+
+import 'package:flutter_way/screens/home/components/special_offers.dart';
 
 import 'package:flutter_way/size_config.dart';
 
+import 'categories.dart';
 import 'home_header.dart';
 
 class Body extends StatelessWidget {
@@ -18,6 +25,29 @@ class Body extends StatelessWidget {
             HomeHeader(),
             SizedBox(height: getProportionateScreenHeight(30)),
             DiscountBanner(),
+            SizedBox(height: getProportionateScreenHeight(30)),
+            Categories(),
+            SizedBox(height: getProportionateScreenHeight(30)),
+            SpecialOffers(),
+            SizedBox(height: getProportionateScreenHeight(30)),
+            SectionTitle(text: 'Popular Products', press: () {}),
+            Column(
+              children: [
+                SizedBox(
+                  width: getProportionateScreenWidth(140),
+                  child: AspectRatio(
+                    aspectRatio: 1.2,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: kSecondaryColor.withOpacity(0.1),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Image.asset(demoProducts[0].images[0]),
+                    ),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
